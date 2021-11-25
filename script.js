bfd39bb188c8baa5cfde4c5432cc1ca4684a283a
pnhnguyen <phannguyenhoainguyen1.com>
2021-11-25 21:38:57 +0700

    blog

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ $('.hamburger').click(function(){
     $(this).toggleClass("active")
     $('.navbar__navlist').toggleClass('active')
 })
+
+//slide 
+$(window).ready(function () {
+    var id = $('.slide.active').attr('id')
+    $('#'+id+'.controls').addClass('active')
+})
+$('.controls').click(function() {
+    $('.controls.active').removeClass('active')
+    $(this).addClass('active')
+    $('.slide.active').removeClass('active')
+    var id = $(this).attr('id')
+    $('#'+id+'.slide').addClass('active')
+})
